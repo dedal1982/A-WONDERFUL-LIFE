7507b176cf904f5f89eb0884dae1b8e1b3259ebb
aldm <d44.a1@yandex.ru>
2024-11-30 19:34:51 +0300

    1

--- swiper.js
+++ swiper.js
@@ -33,3 +33,39 @@ const swiper = new Swiper(".examples__swiper", {
     el: ".swiper-scrollbar",
   },
 });
+
+const swiperReviews = new Swiper(".reviews__swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: false,
+  slidesPerView: 4,
+  spaceBetween: 40,
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 40,
+    },
+    375: {
+      slidesPerView: 1,
+      spaceBetween: 40,
+    },
+    425: {
+      slidesPerView: 1,
+      spaceBetween: 40,
+    },
+  },
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
